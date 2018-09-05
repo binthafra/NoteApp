f314d59b479e596085d52b832d91f3a79002fac7
binthafra <binthafra@gmail.com>
2018-09-06 00:44:28 +0600

    initial commit

--- AndroidStudioProjects/SplashScreen/app/src/main/java/com/example/attention/splashscreen/HomeActivity.java
+++ AndroidStudioProjects/SplashScreen/app/src/main/java/com/example/attention/splashscreen/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.attention.splashscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void Newspaper(View view) {
+        Intent intent = new Intent(HomeActivity.this,NewsPaperActivity.class);
+        startActivity(intent);
     }
 }
